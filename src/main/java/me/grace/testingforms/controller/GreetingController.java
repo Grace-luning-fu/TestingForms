3443aad43beeaf4ace9d1e13f10acaab576070b0
Grace-luning-fu <grace.luning.fu@gmail.com>
2017-08-02 16:44:19 -0400

    validating forms

--- src/main/java/me/grace/testingforms/controller/GreetingController.java
+++ src/main/java/me/grace/testingforms/controller/GreetingController.java
@@ -2,12 +2,16 @@ package me.grace.testingforms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
 @Controller
 public class GreetingController {
+
     @GetMapping("/greeting")
     public String greetingForm(Model model){
         model.addAttribute("greeting", new Greeting());
@@ -15,7 +19,11 @@ public class GreetingController {
     }
 
     @PostMapping("/greeting")
-    public String greetingSubmit(@ModelAttribute Greeting greeting){
+    public String greetingSubmit(@Valid Greeting greeting, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "greeting";
+        }
+
             return "result";
         }
     }
